35288d3f0fa4326aa483a55b7db5ea40c5703ebd
Joe Runde <Joseph.Runde@ibm.com>
2024-03-28 14:20:16 -0600

    :fire: delete unused consts, todo
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- vllm/entrypoints/grpc/grpc_server.py
+++ vllm/entrypoints/grpc/grpc_server.py
@@ -37,11 +37,6 @@ from vllm.transformers_utils.tokenizer_group import BaseTokenizerGroup
 
 logger = init_logger(__name__)
 
-MAX_TOP_N_TOKENS = 10
-
-MAX_STOP_SEQS = 6
-MAX_STOP_SEQ_LENGTH = 240
-
 
 def with_default(value: Any, default: Any) -> Any:
     return value if value else default
