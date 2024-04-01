2559101193a0031c1f37b474782d73e054aedf8b
Max de Bayser <mbayser@br.ibm.com>
2024-04-01 17:57:48 -0300

    Implementation of length penalty using logits processors
    This implementation is simpler and less invasive in hte vllm core.
    It uses the exponanctional decay length penalty from the transformers
    library
    
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

--- vllm/tgis_utils/logits_processors.py
+++ vllm/tgis_utils/logits_processors.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Tuple
 
 import torch
-from transformers.generation.logits_process import TypicalLogitsWarper
+from transformers.generation.logits_process import (
+    ExponentialDecayLengthPenalty, TypicalLogitsWarper)
 
 
 class TypicalLogitsWarperWrapper:
@@ -14,3 +15,19 @@ class TypicalLogitsWarperWrapper:
         # transformers warpers assume tensors of shape (batch_size, vocab_size)
         # and the typical warper doesn't use input_ids
         return self.warper(input_ids=None, scores=logits.reshape((1, -1)))
+
+
+class LengthPenaltyWarper:
+
+    def __init__(self, length_penalty: Tuple[int, float], eos_token_id: int):
+        self.length_penalty = length_penalty
+        self.eos_token_id = eos_token_id
+        self.warper = ExponentialDecayLengthPenalty(
+            exponential_decay_length_penalty=length_penalty,
+            eos_token_id=eos_token_id,
+            input_ids_seq_length=0)
+
+    def __call__(self, token_ids: List[int],
+                 logits: torch.tensor) -> torch.tensor:
+        return self.warper(input_ids=torch.Tensor(token_ids),
+                           scores=logits.reshape((1, -1)))
