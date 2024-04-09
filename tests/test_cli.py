4d1686d7307fedfab46b86facd0aa979d9044d1b
Prashant Gupta <prashantgupta@us.ibm.com>
2024-04-09 15:28:37 -0700

    🎨 format
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,19 @@
 from pathlib import Path
-from vllm.cli import convert_to_fast_tokenizer
-from vllm.tgis_utils.hub import download_weights, get_model_path, local_weight_files
 
+from vllm.cli import convert_to_fast_tokenizer
+from vllm.tgis_utils.hub import (download_weights, get_model_path,
+                                 local_weight_files)
 
 
 def test_convert_to_fast_tokenizer():
     model_name = "EleutherAI/gpt-neo-125m"
-    # make sure to include .json to download the tokenizer.json and tokenizer_config.json
-    files = download_weights(model_name, extension=[".safetensors", ".json"])
+    # make sure to include .json to download the
+    # tokenizer.json and tokenizer_config.json
+    download_weights(model_name, extension=[".safetensors", ".json"])
     model_path = get_model_path(model_name)
-    local_json_files = [Path(p) for p in local_weight_files(model_path, ".json")]
+    local_json_files = [
+        Path(p) for p in local_weight_files(model_path, ".json")
+    ]
     tokenizer_file = [
         file for file in local_json_files if file.name == "tokenizer.json"
     ][0]
@@ -20,11 +24,15 @@ def test_convert_to_fast_tokenizer():
     local_files_remove_tokenizer = [
         Path(p) for p in local_weight_files(model_path, ".json")
     ]
-    assert "tokenizer.json" not in [file.name for file in local_files_remove_tokenizer]
+    assert "tokenizer.json" not in [
+        file.name for file in local_files_remove_tokenizer
+    ]
 
     # this should convert the tokenizer_config.json to tokenizer.json
     convert_to_fast_tokenizer(model_name)
     local_files_with_tokenizer = [
         Path(p) for p in local_weight_files(model_path, ".json")
     ]
-    assert "tokenizer.json" in [file.name for file in local_files_with_tokenizer]
+    assert "tokenizer.json" in [
+        file.name for file in local_files_with_tokenizer
+    ]
