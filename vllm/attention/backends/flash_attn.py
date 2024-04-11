a880d8916d20d4aa66c1e4f485f21bd818165d98
Joe Runde <Joseph.Runde@ibm.com>
2024-04-11 10:05:53 -1000

    Revert "Temp hack to work with prebuilt 0.4.0-post kernels"
    This reverts commit be269f5a22aaac6d16eb4036d62cf5b6a260526e.

--- vllm/attention/backends/flash_attn.py
+++ vllm/attention/backends/flash_attn.py
@@ -189,14 +189,11 @@ class FlashAttentionImpl(AttentionImpl):
             # Reshape the input keys and values and store them in the cache.
             # If kv_cache is not provided, the new key and value tensors are
             # not cached. This happens during the initial memory profiling run.
-            PagedAttention.write_to_paged_cache(
-                key,
-                value,
-                key_cache,
-                value_cache,
-                attn_metadata.slot_mapping,
-                attn_metadata.kv_cache_dtype,
-            )  # kv_scale)
+            PagedAttention.write_to_paged_cache(key, value, key_cache,
+                                                value_cache,
+                                                attn_metadata.slot_mapping,
+                                                attn_metadata.kv_cache_dtype,
+                                                kv_scale)
 
         num_prefill_tokens = attn_metadata.num_prefill_tokens
         num_decode_tokens = attn_metadata.num_decode_tokens
@@ -266,7 +263,7 @@ class FlashAttentionImpl(AttentionImpl):
                 self.num_kv_heads,
                 self.scale,
                 self.alibi_slopes,
-                #kv_scale,
+                kv_scale,
             )
 
         # Reshape the output tensor.
