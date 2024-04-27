0afd2de4644453872cdcb82db62304d96ac0693a
Prashant Gupta <prashantgupta@us.ibm.com>
2024-04-26 19:13:29 -0700

    ⏪ revert py change
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- vllm/model_executor/guided_decoding.py
+++ vllm/model_executor/guided_decoding.py
@@ -65,7 +65,6 @@ async def get_guided_decoding_logits_processor(
     """
     global global_thread_pool
     guide, mode = _get_guide_and_mode(request)
-    # test changing py file
     if not guide:
         return None
 
